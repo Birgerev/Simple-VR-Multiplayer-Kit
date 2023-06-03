912c9fd7789e59e6ad627eb92863f085c0a30ad0
Birger E <bibbevidio@gmail.com>
2023-06-03 18:43:37 +0200

    Working basket

---
+++ Assets/BasketballRange.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Mirror;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(BoxCollider))]
+public class BasketballRange : NetworkBehaviour
+{
+    public Text scoreText;
+    [Space]
+    [SyncVar] public int score;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        scoreText.text = score.ToString();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //When object enters hoop
+        score++;
+    }
+}
